b6e239e77f74e2d79d72b225bbd83cba122f7a94
MikiTran <mikitran721@gmail.com>
2024-02-05 17:42:02 +0700

    next song when ending, mixed with rangeBar

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const song = document.getElementById("song");
 const playBtn = document.querySelector(".play-inner");
 const nextBtn = document.querySelector(".play-forward");
 const prevBtn = document.querySelector(".play-back");
+// timer
+const durationTime = document.querySelector(".duration");
+const remainingTime = document.querySelector(".remaining");
+const rangeBar = document.querySelector(".range");
+
 let isPlaying = true;
 const musics = [
   "cohuong-up.m4a",
@@ -13,6 +18,10 @@ const musics = [
 ];
 let indexSong = 0;
 
+displayTimer();
+// hien thi time chay nhac
+// let timer = setInterval(displayTimer, 500);
+let timer;
 song.setAttribute("src", `./music/${musics[indexSong]}`);
 
 nextBtn.addEventListener("click", function () {
@@ -23,6 +32,12 @@ prevBtn.addEventListener("click", function () {
   changeSong(-1);
 });
 
+// next song when ending
+song.addEventListener("ended", handleEndedSong);
+
+function handleEndedSong() {
+  changeSong(1);
+}
 //function changeSong
 function changeSong(dir) {
   if (dir === 1) {
@@ -51,9 +66,40 @@ function playPause() {
     song.play();
     isPlaying = false;
     playBtn.innerHTML = `<ion-icon name="pause-circle"></ion-icon>`;
+    timer = setInterval(displayTimer, 500);
   } else {
     song.pause();
     isPlaying = true;
     playBtn.innerHTML = `<ion-icon name="play"></ion-icon>`;
+    clearInterval(timer);
   }
+}
+
+// timer function
+function displayTimer() {
+  const { duration, currentTime } = song;
+  rangeBar.max = duration;
+  rangeBar.value = currentTime;
+  remainingTime.textContent = formatTimer(currentTime);
+  if (!duration) {
+    durationTime.textContent = "00:00";
+  } else {
+    durationTime.textContent = formatTimer(duration);
+  }
+}
+
+// format number
+function formatTimer(number) {
+  const minutes = Math.floor(number / 60);
+  const seconds = Math.floor(number - minutes * 60);
+  return `${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+}
+
+//change rangeBar
+rangeBar.addEventListener("change", handleChangeBar);
+function handleChangeBar() {
+  // rangeBar.value = song.currentTime;
+  song.currentTime = rangeBar.value;
 }
