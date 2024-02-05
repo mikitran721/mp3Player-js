c32855b13773c226d11e3c2961d13c11fa11c679
MikiTran <mikitran721@gmail.com>
2024-02-05 14:08:19 +0700

    function for press next-prev

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
+/** clone from EVONDEV */
+
 const song = document.getElementById("song");
 const playBtn = document.querySelector(".play-inner");
+const nextBtn = document.querySelector(".play-forward");
+const prevBtn = document.querySelector(".play-back");
 let isPlaying = true;
+const musics = [
+  "cohuong-up.m4a",
+  "YuGang-TruongXuan.m4a",
+  "Zhongguo李玉刚华夏温暖入心.mp3",
+  "绕ㅡ李玉刚.mp3",
+];
+let indexSong = 0;
+
+song.setAttribute("src", `./music/${musics[indexSong]}`);
+
+nextBtn.addEventListener("click", function () {
+  changeSong(1);
+});
+
+prevBtn.addEventListener("click", function () {
+  changeSong(-1);
+});
+
+//function changeSong
+function changeSong(dir) {
+  if (dir === 1) {
+    // next song
+    indexSong++;
+    if (indexSong >= musics.length) {
+      indexSong = 0;
+    }
+    isPlaying = true;
+  } else if (dir === -1) {
+    // prev song
+    indexSong--;
+    if (indexSong < 0) {
+      indexSong = musics.length - 1;
+    }
+    isPlaying = true;
+  }
+  song.setAttribute("src", `./music/${musics[indexSong]}`);
+  playPause();
+}
 
 playBtn.addEventListener("click", playPause);
 
